c51084dd540de10841f7f71b5b930bfeb1109bf7
Anas <anashossamhafez@gmail.com>
2026-05-17 15:54:45 +0300

    Added "Resource Monitor" project

--- Resource Monitor/pulse.py
+++ Resource Monitor/pulse.py
@@ -1,9 +1,18 @@
-import psutil as ps
-import time 
-import os 
+import psutil as ps #for resource monitoring
+import time
+import subprocess #allows us to execute system wide commands
 
-while(True):
-    os.system('clear')
-    print(f"usage: {ps.cpu_percent()}%")
-    print(f"frequency: {ps.cpu_freq(percpu=False)}")
-    time.sleep(1)
+
+def main(): #now the code is containerized, not run directly by running the file
+    while(True):
+ 
+        subprocess.call("clear")
+
+        print(f"usage: {ps.cpu_percent()}%")
+
+        print(f"frequency: {ps.cpu_freq(percpu=False)}")
+
+        time.sleep(1)
+
+if __name__ == "__main__":#checks if the file is being run directly, not imported
+    main()
